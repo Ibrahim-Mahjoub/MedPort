6f9389ca2f93fcc548ca7201a11ae931632e3b6a
Ibrahim Mahjoub <imahjoub@live.ca>
2020-12-22 14:26:04 -0500

    removed redundant files

--- helpers/referral.js
+++
@@ -1,9 +0,0 @@
-const referralCodeLength = 6;
-
-/* Referral Helper Functions */
-const generateReferralCode = () => {
-	return Math.random().toString(36).substr(2, referralCodeLength).toUpperCase()
-};
-
-module.exports = { generateReferralCode };
-
