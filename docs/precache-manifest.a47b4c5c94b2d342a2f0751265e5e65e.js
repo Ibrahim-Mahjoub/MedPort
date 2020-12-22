6310bfe3d0fa0add2ca9ed48db473dce88656a23
Ibrahim Mahjoub <imahjoub@live.ca>
2020-12-22 15:34:18 -0500

    added production build files

---
+++ docs/precache-manifest.a47b4c5c94b2d342a2f0751265e5e65e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f1671147e7f8e49dd2ef2822ddb189",
+    "url": "/medport/index.html"
+  },
+  {
+    "revision": "4833da484d9c7ebe6534",
+    "url": "/medport/static/css/2.9a547d2e.chunk.css"
+  },
+  {
+    "revision": "ee930020ecf98c9f42e0",
+    "url": "/medport/static/css/main.50c6ef94.chunk.css"
+  },
+  {
+    "revision": "4833da484d9c7ebe6534",
+    "url": "/medport/static/js/2.c87c2d3c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/medport/static/js/2.c87c2d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee930020ecf98c9f42e0",
+    "url": "/medport/static/js/main.db2d7683.chunk.js"
+  },
+  {
+    "revision": "d78d190afdb9e6124ebc",
+    "url": "/medport/static/js/runtime-main.d423ea42.js"
+  },
+  {
+    "revision": "da245a3efc62ddacf96ae7cb038fdd19",
+    "url": "/medport/static/media/dashboard-picture.da245a3e.png"
+  },
+  {
+    "revision": "102383403b2ed906b8fa3396f6133c83",
+    "url": "/medport/static/media/default-profile-icon.10238340.png"
+  },
+  {
+    "revision": "abe47494e170ff00caeb281f08769888",
+    "url": "/medport/static/media/home-bg-doctor-patient-online.abe47494.png"
+  },
+  {
+    "revision": "60e6efcda98ed50de006d059f894e8fc",
+    "url": "/medport/static/media/opaque-upload-profile.60e6efcd.png"
+  },
+  {
+    "revision": "2a1c7262e38dd37a8ef1b5c36dcf461c",
+    "url": "/medport/static/media/pencil-edit-icon.2a1c7262.png"
+  }
+]);
